83fd9c70fc1ac2700c63a089f33c9a2ab4fd84ec
mgarcia1626 <matiasegarcia97@gmail.com>
2025-01-06 18:30:04 -0300

    2nd

--- CHATBOT/start/whatsapp_quickstart.py
+++ CHATBOT/start/whatsapp_quickstart.py
@@ -46,94 +46,95 @@ print(response.json())
 
 # --------------------------------------------------------------
 # Send a custom text WhatsApp message
-# --------------------------------------------------------------
-
-# NOTE: First reply to the message from the user in WhatsApp!
-
-
-def get_text_message_input(recipient, text):
-    return json.dumps(
-        {
-            "messaging_product": "whatsapp",
-            "recipient_type": "individual",
-            "to": recipient,
-            "type": "text",
-            "text": {"preview_url": False, "body": text},
-        }
-    )
-
-
-def send_message(data):
-    headers = {
-        "Content-type": "application/json",
-        "Authorization": f"Bearer {ACCESS_TOKEN}",
-    }
-
-    url = f"https://graph.facebook.com/{VERSION}/{PHONE_NUMBER_ID}/messages"
-
-    response = requests.post(url, data=data, headers=headers)
-    if response.status_code == 200:
-        print("Status:", response.status_code)
-        print("Content-type:", response.headers["content-type"])
-        print("Body:", response.text)
-        return response
-    else:
-        print(response.status_code)
-        print(response.text)
-        return response
-
-
-data = get_text_message_input(
-    recipient=RECIPIENT_WAID, text="Hello, this is a test message."
-)
-
-response = send_message(data)
-
-# --------------------------------------------------------------
-# Send a custom text WhatsApp message asynchronously
-# --------------------------------------------------------------
-
-
-# Does not work with Jupyter!
-async def send_message(data):
-    headers = {
-        "Content-type": "application/json",
-        "Authorization": f"Bearer {ACCESS_TOKEN}",
-    }
-
-    async with aiohttp.ClientSession() as session:
-        url = "https://graph.facebook.com" + f"/{VERSION}/{PHONE_NUMBER_ID}/messages"
-        try:
-            async with session.post(url, data=data, headers=headers) as response:
-                if response.status == 200:
-                    print("Status:", response.status)
-                    print("Content-type:", response.headers["content-type"])
-
-                    html = await response.text()
-                    print("Body:", html)
-                else:
-                    print(response.status)
-                    print(response)
-        except aiohttp.ClientConnectorError as e:
-            print("Connection Error", str(e))
-
-
-def get_text_message_input(recipient, text):
-    return json.dumps(
-        {
-            "messaging_product": "whatsapp",
-            "recipient_type": "individual",
-            "to": recipient,
-            "type": "text",
-            "text": {"preview_url": False, "body": text},
-        }
-    )
-
-
-data = get_text_message_input(
-    recipient=RECIPIENT_WAID, text="Hello, this is a test message."
-)
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(send_message(data))
-loop.close()
+## --------------------------------------------------------------
+#
+## NOTE: First reply to the message from the user in WhatsApp!
+#
+#
+#def get_text_message_input(recipient, text):
+#    return json.dumps(
+#        {
+#            "messaging_product": "whatsapp",
+#            "recipient_type": "individual",
+#            "to": recipient,
+#            "type": "text",
+#            "text": {"preview_url": False, "body": text},
+#        }
+#    )
+#
+#
+#def send_message(data):
+#    headers = {
+#        "Content-type": "application/json",
+#        "Authorization": f"Bearer {ACCESS_TOKEN}",
+#    }
+#
+#    url = f"https://graph.facebook.com/{VERSION}/{PHONE_NUMBER_ID}/messages"
+#
+#    response = requests.post(url, data=data, headers=headers)
+#    if response.status_code == 200:
+#        print("Status:", response.status_code)
+#        print("Content-type:", response.headers["content-type"])
+#        print("Body:", response.text)
+#        return response
+#    else:
+#        print(response.status_code)
+#        print(response.text)
+#        return response
+#
+#
+#data = get_text_message_input(
+#    recipient=RECIPIENT_WAID, text="Hello, this is a test message."
+#)
+#
+#response = send_message(data)
+#
+## --------------------------------------------------------------
+## Send a custom text WhatsApp message asynchronously
+## --------------------------------------------------------------
+#
+#
+## Does not work with Jupyter!
+#async def send_message(data):
+#    headers = {
+#        "Content-type": "application/json",
+#        "Authorization": f"Bearer {ACCESS_TOKEN}",
+#    }
+#
+#    async with aiohttp.ClientSession() as session:
+#        url = "https://graph.facebook.com" + f"/{VERSION}/{PHONE_NUMBER_ID}/messages"
+#        try:
+#            async with session.post(url, data=data, headers=headers) as response:
+#                if response.status == 200:
+#                    print("Status:", response.status)
+#                    print("Content-type:", response.headers["content-type"])
+#
+#                    html = await response.text()
+#                    print("Body:", html)
+#                else:
+#                    print(response.status)
+#                    print(response)
+#        except aiohttp.ClientConnectorError as e:
+#            print("Connection Error", str(e))
+#
+#
+#def get_text_message_input(recipient, text):
+#    return json.dumps(
+#        {
+#            "messaging_product": "whatsapp",
+#            "recipient_type": "individual",
+#            "to": recipient,
+#            "type": "text",
+#            "text": {"preview_url": False, "body": text},
+#        }
+#    )
+#
+#
+#data = get_text_message_input(
+#    recipient=RECIPIENT_WAID, text="Hello, this is a test message."
+#)
+#
+#loop = asyncio.get_event_loop()
+#loop.run_until_complete(send_message(data))
+#loop.close()
+#
