f1d159df184fe018bbf4340393e60e86dec07b20
mgarcia1626 <matiasegarcia97@gmail.com>
2025-01-11 20:25:42 -0300

    Save updated to drive

---
+++ CHATBOT/Call.py
@@ -0,0 +1,27 @@
+from googleapiclient.http import MediaFileUpload
+from CHATBOT.app.utils.Google import Create_Service
+
+CLIENT_SECRET_FILE = "AAAAA.json"
+API_NAME = "drive"
+API_VERSION = "v3"
+SCOPES = ["https://www.googleapis.com/auth/drive"]
+
+service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+
+folder_id = "1YxgD2ydEpA65A4_QWLPglYcYPH-DYU-9"
+file_names = ["socios.xlsx"]
+mime_types = ["application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"]
+
+for file_name, mime_type in zip(file_names, mime_types):
+    file_metadata = {
+        "name" : file_name,
+        "parents" : [folder_id]
+    }
+
+    media = MediaFileUpload("{0}".format(file_name), mimetype=mime_type)
+
+    service.files().create(
+        body=file_metadata,
+        media_body = media,
+        fields = "id"
+    ).execute()
--- CHATBOT/app/utils/variables.py
+++ CHATBOT/app/utils/variables.py
@@ -1,6 +1,8 @@
 
 import pandas as pd
 import openpyxl
+from googleapiclient.http import MediaFileUpload
+from Google import Create_Service
 
 #Count es para saber donde estas.
 count=0
@@ -115,6 +117,32 @@ nuevo_Socio = socio(None, None, None, None, None, None, None, None, None, None, 
 
 
 # Crear una lista de diccionarios con los atributos de los objetos
+def Update_To_Drive():
+    CLIENT_SECRET_FILE = "AAAAA.json"
+    API_NAME = "drive"
+    API_VERSION = "v3"
+    SCOPES = ["https://www.googleapis.com/auth/drive"]
+
+    service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+
+    folder_id = "1YxgD2ydEpA65A4_QWLPglYcYPH-DYU-9"
+    file_names = ["socios.xlsx"]
+    mime_types = ["application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"]
+
+    for file_name, mime_type in zip(file_names, mime_types):
+        file_metadata = {
+            "name" : file_name,
+            "parents" : [folder_id]
+        }
+
+        media = MediaFileUpload("{0}".format(file_name), mimetype=mime_type)
+
+        service.files().create(
+            body=file_metadata,
+            media_body = media,
+            fields = "id"
+        ).execute()
+
 
 def data_socio(usuario):
 
@@ -286,6 +314,7 @@ def Decision_func(count,message_body,ErrorCounter):
         elif count==18 and message_body=="Si":
             Toadd=agregar_aLista(nuevo_Socio)
             data_socio(Toadd)
+            Update_To_Drive()
             ToSend="Gracias , recuerde que no tenemos natacion"
             Counter=0
 
